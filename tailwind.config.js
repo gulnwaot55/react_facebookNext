ccef9b71f13a6cba561e940bb45ef883136f69c0
gulnwaot55 <aotzatingtong5@gmail.com>
2023-02-06 15:43:31 +0700

    colorTheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,13 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  themes: [
+    {
+      mytheme: {
+        primary: "#4267B2", //logo & icon
+        search_navbar: "#898F9C", // search_navbar
+        background: "#000000", // background
+      },
+    },
+  ],
 }
