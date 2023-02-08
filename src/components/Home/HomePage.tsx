1983cecd3ed55f95b4dad87674b53600ef9bcb78
kanyanat <kanyanat.kjunior@gmail.com>
2023-02-09 02:15:52 +0700

    add ui post

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import ProfileDetail from "./ProfileDetail";
-import ProfileUser from "@/layouts/ProfileUser";
 import { tebsMenu, tebsMenuPost } from "@/models/Constant/TabsMene";
 import CardImg from "@/layouts/CardImg";
 import { dataList } from "@/models/Constant/StoryImages";
@@ -14,35 +13,36 @@ export default function HomePage() {
   const Text = () => <div>You clicked the button!</div>;
   return (
     <>
-      <div className="flex  bg-zinc-100 ">
-        <div className="flex basis-1/4  flex-row   items-center ml-4 mx-10 my-6 ">
+      <div className="flex bg-zinc-100  ">
+        <div className="flex grow basis-1/4  flex-row   items-center ml-4 mx-10 p-10 ">
           <ProfileDetail />
         </div>
-        <div className="flex flex-col basis-1/4  rounded-lg my-6   drop-shadow-lg">
+        <div className="flex grow flex-col basis-1/3  rounded-lg my-6 mx-10  drop-shadow-lg">
           <div className=" flex flex-row w-full  items-center justify-around border-b-2  bg-white  rounded-t-lg">
             {tebsMenu.map((data) => {
               return <TabsMenuBar name={data.name} img={data.icon} />;
             })}
+            
           </div>
-          <div className="flex grow flex-row bg-white h-1 rounded-b-lg ">
+          {/* <div className="flex  flex-row bg-white rounded-b-lg ">
             {dataList.map((data) => {
               return <CardImg name={data.name} img={data.img} />;
             })}
-          </div>
+          </div> */}
           <div className=" bg-white mt-4 rounded-lg p-4 ">
             <div className="flex flex-row grow  border-b-4 pb-6">
               {UserProfile.map((data) => {
                 return <AvatarImg img={data.image} />;
               })}
-              <div>
-                <input
-                  type="email"
-                  id="helper-text"
-                  aria-describedby="helper-text-explanation"
-                  className=" w-full text-gray-900  rounded-full    p-2.5  dark:bg-gray-200 "
-                  placeholder={"คุณคิดอะไรอยู่" + { UserProfile }}
-                />
-              </div>
+              <input
+                type="email"
+                id="helper-text"
+                aria-describedby="helper-text-explanation"
+                className=" w-full  text-gray-900  rounded-full    p-2.5  dark:bg-gray-200 "
+                placeholder={
+                  "คุณคิดอะไรอยู่ " + { UserProfile }.UserProfile[0].name
+                }
+              />
             </div>
             <div className="flex flex-row grow items-baseline   justify-around">
               {tebsMenuPost.map((data) => {
@@ -50,29 +50,20 @@ export default function HomePage() {
               })}
             </div>
           </div>
-        <div>
-        <div className=" bg-white mt-4 rounded-lg p-4 ">
-            <div className="flex flex-row grow  border-b-4 pb-6">
-              {UserProfile.map((data) => {
-                return <AvatarImg img={data.image} />;
-              })}
-              <div>
-                <input
-                  type="email"
-                  id="helper-text"
-                  aria-describedby="helper-text-explanation"
-                  className=" w-full text-gray-900  rounded-full    p-2.5  dark:bg-gray-200 "
-                  placeholder={"คุณคิดอะไรอยู่" + { UserProfile }}
-                />
+          <div>
+            <div className=" bg-white mt-4 rounded-lg p-4 ">
+              <div className="flex flex-row grow items-center pb-6 ">
+                {UserProfile.map((data) => {
+                  return <AvatarImg img={data.image} />;
+                })}
+                <div className=" flex flex-col ">
+                  <p> {UserProfile[0].name}</p>
+                  <p> 1 วัน </p>
+                </div>
               </div>
-            </div>
-            <div className="flex flex-row grow items-baseline   justify-around">
-              {tebsMenuPost.map((data) => {
-                return <TabsMenuBar name={data.name} img={data.icon} />;
-              })}
+              <img src="https://i.pinimg.com/originals/6e/62/58/6e62587d5c63620ce8deaa448b30accf.jpg" />
             </div>
           </div>
-        </div>
         </div>
         <div className="flex grow basis-1/4 flex-col  justify-end   items-center "></div>
       </div>
