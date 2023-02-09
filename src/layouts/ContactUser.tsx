18b29458b131809754921e29f0c5e6b1cc28bb8e
kanyanat <kanyanat.kjunior@gmail.com>
2023-02-09 16:39:20 +0700

    cycle bg img

--- src/layouts/ContactUser.tsx
+++ src/layouts/ContactUser.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import Image from "next/image";
 
 interface Props {
-    name: string;
-    img: string;
+  name: string;
+  img: string;
 }
 
 export default function ContactUser({ name, img }: Props) {
-    return (
-        <div className="hover:bg-stone-200 hover:rounded-lg py-1 px-1 w-full">
-            <div className="avatar">
-                <div className="avatar online  ">
-                    <div className="w-12 rounded-full ">
-                        <img src={img} />
-                    </div>
-                </div>
-                <h1 className="text-black pl-4 py-2 font-medium"> {name}</h1>
-            </div>
+  return (
+    <div className="hover:bg-stone-200 hover:rounded-lg py-1 px-1 w-full flex ">
+      <div className="avatar online ">
+        <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+          <img src={img} />
         </div>
-    );
+      </div>
+      <h1 className="flex text-black pl-4 py-2 font-medium items-center justify-center ">
+        {" "}
+        {name}
+      </h1>
+    </div>
+  );
 }
-
-
