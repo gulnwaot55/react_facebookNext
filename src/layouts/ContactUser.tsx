a731e623c829e24d2b45ae954824295f600a6c1a
Thossapol Seekram <gulnwaot55>
2023-02-09 16:33:15 +0700

    RightUp

--- src/layouts/ContactUser.tsx
+++ src/layouts/ContactUser.tsx
@@ -8,16 +8,12 @@ interface Props {
 
 export default function ContactUser({ name, img }: Props) {
     return (
-        <div className="hover:bg-slate-100 hover:rounded-lg py-1 px-1 w-full">
-            <div className="avatar ">
-                <div className="w-10 h-10 rounded-full">
-                    <Image
-                        className=" w-full h-full rounded-md object-cover"
-                        height={80}
-                        width={80}
-                        alt={name}
-                        src={img}
-                    />
+        <div className="hover:bg-stone-200 hover:rounded-lg py-1 px-1 w-full">
+            <div className="avatar">
+                <div className="avatar online  ">
+                    <div className="w-12 rounded-full ">
+                        <img src={img} />
+                    </div>
                 </div>
                 <h1 className="text-black pl-4 py-2 font-medium"> {name}</h1>
             </div>
